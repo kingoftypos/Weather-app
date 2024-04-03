95dc7eb0269ac2acea3ae998bc229d905d410765
Shreyansh mishra <mshreyansh121@gmail.com>
2024-04-03 19:18:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey="23ca7b83d11c29556338bf640042c100";
+const apiKey="2a27152e6f687aaa52ef3c0690b03f8fd";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=city=Germany";
 
 async function checkWeather(){
